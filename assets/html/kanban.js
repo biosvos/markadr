881cfe808de899d889b0fc90853affc582cab1dd
jinsu <realwaterkim@datacommand.kr>
2023-07-17 12:17:36 +0900

    change status if file changed

--- assets/html/kanban.js
+++ assets/html/kanban.js
@@ -1,3 +1,5 @@
+let adrs = {};
+
 function addTaskEvent(obj) {
     obj.on("dragstart", function (event) {
         event.originalEvent.dataTransfer.setData("text", event.target.id);
@@ -7,24 +9,41 @@ function addTaskEvent(obj) {
     })
 }
 
-$(function () {
-    function getDivByStatus(status) {
-        switch (status) {
-            case "Draft":
-                return $("#draft");
-            case "Proposed":
-                return $("#proposed");
-            case "Rejected":
-                return $("#rejected");
-            case "Accepted":
-                return $("#accepted");
-            case "Deprecated":
-                return $("#deprecated");
-            case "Superseded":
-                return $("#superseded");
-        }
+function getDivByStatus(status) {
+    switch (status) {
+        case "Draft":
+            return $("#draft");
+        case "Proposed":
+            return $("#proposed");
+        case "Rejected":
+            return $("#rejected");
+        case "Accepted":
+            return $("#accepted");
+        case "Deprecated":
+            return $("#deprecated");
+        case "Superseded":
+            return $("#superseded");
     }
+}
 
+function getStatusByID(id) {
+    switch (id) {
+        case "draft":
+            return "Draft";
+        case "proposed":
+            return "Proposed";
+        case "rejected":
+            return "Rejected";
+        case "accepted":
+            return "Accepted";
+        case "deprecated":
+            return "Deprecated";
+        case "superseded":
+            return "Superseded";
+    }
+}
+
+$(function () {
     $.ajax({
         contentType: 'application/json',
         url: `/summaries`,
@@ -35,10 +54,11 @@ $(function () {
             let task = $(`<div class="task" id="${summary["title"]}" draggable="true"><span>${summary["title"]}</span></div>`);
             div.append(task);
             addTaskEvent(task);
+
+            adrs[summary["title"]] = summary;
         })
     });
 })
-
 
 $(function () {
     let tasks = $(".task");
@@ -47,16 +67,24 @@ $(function () {
     let blocks = $(".kanban-block");
     blocks.on("drop", function (event) {
         event.preventDefault();
-        let data = event.originalEvent.dataTransfer.getData("text");
-        event.originalEvent.currentTarget.appendChild(document.getElementById(data));
+        let title = event.originalEvent.dataTransfer.getData("text");
+        event.originalEvent.currentTarget.appendChild(document.getElementById(title));
+        let statusID = event.originalEvent.currentTarget.id;
+        let status = getStatusByID(statusID)
+
         $.ajax({
             contentType: 'application/json',
-            url: `/pages/${data}`,
+            url: `/pages/${title}`,
             type: 'put',
             data: JSON.stringify({
-                status: event.originalEvent.currentTarget.id
+                status: statusID
             })
-        });
+        }).done(function () {
+            adrs[title] = {
+                "title": title,
+                "status": status
+            };
+        })
     });
     blocks.on("dragover", function (event) {
         event.preventDefault();
@@ -67,8 +95,32 @@ $(function () {
     const client = mqtt.connect("ws://127.0.0.1:9001");
     client.subscribe("adr");
     client.on("message", function (topic, payload) {
-        console.log(payload);
-        console.log(JSON.parse(payload));
+        let adr = JSON.parse(payload);
+        console.log(adr);
+        let title = adr["title"];
+        let status = adr["status"];
+        let adrElement = $(`#${title}`);
+        if (status === "") { // deleted
+            adrElement.detach();
+            adrs.delete(`${title}`);
+            return;
+        }
+
+        let div = getDivByStatus(adr["status"]);
+        if (!Object.keys(adrs).includes(title)) { // created
+            let task = $(`<div class="task" id="${title}" draggable="true"><span>${title}</span></div>`);
+            div.append(task);
+            addTaskEvent(task);
+            adrs[title] = adr;
+            return;
+        }
+
+        // updated
+        if (adrs[title]["status"] === adr["status"]) { // same
+            return;
+        }
+        adrElement.detach();
+        div.append(adrElement);
+        adrs[title] = adr;
     });
 });
-
